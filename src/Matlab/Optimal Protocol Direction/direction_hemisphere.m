696ebfe3d2f6ef96fe08912642cf6f0d846e1c04
Mayo <mayo.faulkner@ucl.ac.uk>
2018-06-27 13:11:54 +0100

    new direction code

--- src/Matlab/Optimal Protocol Direction/direction_hemisphere.m
+++ src/Matlab/Optimal Protocol Direction/direction_hemisphere.m
@@ -1,75 +1,57 @@
-%%
-clear all
-load('direction_test.mat');
-
-start_method = 'B'; %A for first start search, B for alternative start of search
-%A good way to decide which option is better is to look at total_j and
-%zero_bins - want total_j to be as big as possible but don't want too many
-%zero bins
-
-%%
-n = 114;
+n = 52;
 
 M = w_mag(1:n,1:n);
 X = w_x(1:n,1:n);
 Y = w_y(1:n,1:n);
 Z = w_z(1:n,1:n);
 
-%Convert from cartesian to spherical coordinates
-%Take abs of all values as we only want to look in 1/8 of sphere
+
 for j = 1:n
     for k = 1:n
         w_cart{j,k} = [X(j,k),Y(j,k),Z(j,k)];
         rho(j,k) = norm([X(j,k), Y(j,k), Z(j,k)],2);
-        phi(j,k) = atan(abs(Y(j,k))/abs(X(j,k)))*(180/pi);
-        theta(j,k) = acos(abs(Z(j,k))/rho(j,k))*(180/pi);
+        
+        if X(j,k) < 0
+            X(j,k) = -1*X(j,k);
+            Y(j,k) = -1*Y(j,k);
+            Z(j,k) = -1*Z(j,k);
+        phi(j,k) = atan((X(j,k))/(Y(j,k)))*(180/pi);
+        theta(j,k) = acos((Z(j,k))/rho(j,k))*(180/pi);
+     
+        else
+        phi(j,k) = atan((X(j,k))/(Y(j,k)))*(180/pi);
+        theta(j,k) = acos((Z(j,k))/rho(j,k))*(180/pi);
+    end
         
         w_spher{j,k} = [rho(j,k),phi(j,k),theta(j,k)];
     end
 end
 
-%%
-%Define the number of angles you want to segment into
-%Number of bins will be n_angle*n_angle as we segment in both theta and phi
-%direction
-n_angles = 9;
-d_angle = 90/n_angles;
 
-%Find all protocols that lie in each bin
-for iphi = 1:n_angles
-    for itheta = 1:n_angles
-        idx{iphi,itheta} = find(phi >= (iphi-1)*d_angle & phi < iphi*d_angle & theta >= (itheta - 1)*d_angle & theta < itheta*d_angle);
+n_angles = 6;
+d_angle = 180/n_angles;
+%phi goes from 0 to 90 and then -90 to 0
+%theta goes from  0 to 180
+
+phi_seg = [0:d_angle:90, -90:d_angle:0];
+theta_seg = [0:d_angle:180];
+
+for iphi = 1:length(phi_seg)-1
+    for itheta = 1:length(theta_seg)-1
+        idx{iphi,itheta} = find(phi >= phi_seg(iphi) & phi < phi_seg(iphi+1) & theta >= theta_seg(itheta) & theta < theta_seg(itheta+1));
     end
 end
+
+idx( all(cellfun(@isempty,idx),2), : ) = [];
+
 %%
-%Some bins don't contain any protocol lines so remove these
 idx = reshape(idx, n_angles*n_angles,1);
 rm = find(cellfun(@isempty,idx));
 idx(rm) = [];
 
 n_bins = size(idx,1);
 
-%%
-if start_method == 'A'
-%Find the protocol with the maximum magnitude in each bin and sort from
-%largest to smallest
-%Determines the order with which we look through the bins as later ones
-%will be more constrained as fewer allowable electrodes left
 
-for iMag = 1:n_bins
-max_j(iMag) = max(M(idx{iMag,1}));
-end
-
-[~,I] = sort(max_j, 'descend');
-
-end
-%%
-if start_method == 'B'
-%This is an alternative option to start the search
-%Finds the total number of possible electrodes that can be used to address
-%a bin
-%The lower the number the fewer possibilities so better to start with these
-%first to ensure there is at least one protocol that targets that bin
 for i = 1:n_bins
     [R,C] = ind2sub(size(theta), idx{i,1}(:));
     diff_elec = unique([R;C]);
@@ -78,13 +60,6 @@ end
 
 [~,I] = sort(cellfun(@length,elec));
 
-end
-
-%%
-%First go through each bin and find the best protocol line, making sure
-%they are all independent
-
-%Store value of total current density in each bin
 j_bins = zeros(n_bins,1);
 
 for iProt = 1:n_bins
@@ -120,10 +95,6 @@ M(:,A) = 0;
 end
 
 %%
-%Now fill out the rest of the protocol
-%Look at the bins that have the smallest current density and add to these
-%to try and make magnitude as evenly spread out as possible
-
 for iProt = n_bins:n-1
 %    for iProt = 1:n-1
 
@@ -193,19 +164,13 @@ for i= 1:n-1
 end
 
 %%
-%Plot the vectors
 figure
-for i = 1:n-1
-quiver3(0,abs(w_x(prt(i,1),prt(i,2))), abs(w_y(prt(i,1),prt(i,2))), abs(w_z(prt(i,1),prt(i,2))));
-hold on;
-end
-
-%%
-%Plot normalised vectors within unit sphere to make sure they are covering
-%1/8th of area of sphere
-figure
-for i = 1:n-1
-quiver3(0,0,0,abs(w_x(prt(i,1),prt(i,2))/rho(prt(i,1),prt(i,2))), abs(w_y(prt(i,1),prt(i,2))/rho(prt(i,1),prt(i,2))), abs(w_z(prt(i,1),prt(i,2))/rho(prt(i,1),prt(i,2))));
+for i = 3%:n-1
+    if w_x(prt(i,1),prt(i,2)) < 0
+quiver3(0,0,0,-1*(w_x(prt(i,1),prt(i,2))/rho(prt(i,1),prt(i,2))),-1*(w_y(prt(i,1),prt(i,2))/rho(prt(i,1),prt(i,2))),-1*(w_z(prt(i,1),prt(i,2))/rho(prt(i,1),prt(i,2))));
+    else
+        quiver3(0,0,0,(w_x(prt(i,1),prt(i,2))/rho(prt(i,1),prt(i,2))),(w_y(prt(i,1),prt(i,2))/rho(prt(i,1),prt(i,2))),(w_z(prt(i,1),prt(i,2))/rho(prt(i,1),prt(i,2))));
+    end
 hold on;
 end
 
@@ -217,6 +182,7 @@ shading interp
 daspect([1 1 1]);
 axis vis3d
 set(gca,'XDir','rev','YDir','rev');
+
 %%
 %Plots a heat map on eighth sphere
 %N.B have changed contents of sphere3D so the colour scale is correct
@@ -236,19 +202,16 @@ else
 end
 
 j_idx = reshape(j_idx, n_angles, n_angles);
-    
 
 %%
 coord = zeros(n_angles+1,n_angles+1);
 coord(1:end-1,1:end-1) = j_idx;
 figure;
-[xout,yout,zout,cout]=sphere3d(coord,0,pi/2,0,pi/2,1,1, 'surf',.000001);
+[xout,yout,zout,cout]=sphere3d(coord,0,pi,0,pi,1,1, 'surf',.0000000000001); %original .000001
 
 cout = cout(2:end,1:end-1);
 
-
 %%
-% Really haccky
 figure;
 for ix = 1:n_angles
     for iy = 1:n_angles
